920edb8443f3256159f54a616e1726f9a33d91ca
Andrew Niekamp and russ vetsper <andrew+russ@>
2016-07-18 12:37:19 -0700

    add a test method for CanAttack on matching X coords

--- Objects/Queen.cs
+++ Objects/Queen.cs
@@ -4,13 +4,20 @@ namespace ChessMove.Objects
 {
   public class Queen
   {
-    private int _x_pos;
-    private int _y_pos;
+    private int _x_queen;
+    private int _y_queen;
 
     public Queen(int x, int y)
     {
-      _x_pos = x;
-      _y_pos = y;
+      _x_queen = x;
+      _y_queen = y;
+    }
+
+    public bool CanAttack(int x, int y)
+    {
+      int _x_other = x;
+      int _y_other = y;
+      return false;
     }
   }
 }
--- Tests/QueenTest.cs
+++ Tests/QueenTest.cs
@@ -1,9 +1,15 @@
 using Xunit;
+using ChessMove.Objects;
 
 namespace ChessMove
 {
   public class ChessMoveTest
   {
-    
+    [Fact]
+    public void CanAttack_ForMatchingX_true()
+    {
+      Queen testQueen = new Queen(4,1);
+      Assert.Equal(true, testQueen.CanAttack(4, 5));
+    }
   }
 }
